8a1796e4fa0ca3408b58be79b0195d7fd11fa6f9
Sergio Ortega Gomez <sergio.ortega.gomez@gmail.com>
2018-12-26 16:46:34 +0100

    Add: Add logManaer

---
+++ src/managers/logManager.js
@@ -0,0 +1,19 @@
+var debug = require('debug')('app:logManager')
+var LogModel = require('../models/logModel')
+
+class LogManager {
+    constructor() {
+        this.save = function(data, next) {
+            debug("Save: ", data)
+            var log = new LogModel(data)
+            log.save(function(err) {
+                if (err) debug(err)
+                next((err ? "ERROR": "OK"), (err ? err.message : ""))
+            })            
+        }
+    }
+}
+
+let logManager = new LogManager()
+debug("LogManager loaded...")
+module.exports = logManager
